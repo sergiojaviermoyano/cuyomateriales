5516f5638cbec02ec99e05ab4ef60382f25a59d8
Sergio Moyano <sergio.moyano@outlook.com.ar>
2017-05-08 23:08:16 -0300

    agregar crud de subrubros

--- application/controllers/Rubro.php
+++ application/controllers/Rubro.php
@@ -16,7 +16,6 @@ class rubro extends CI_Controller {
 		$data['permission'] = $permission;
 		echo json_encode($this->load->view('rubros/list', $data, true));
 	}
-
 	
 	public function getRubro(){
 		$data['data'] = $this->Rubros->getRubro($this->input->post());
@@ -26,6 +25,32 @@ class rubro extends CI_Controller {
 
 	public function setRubro(){
 		$data = $this->Rubros->setRubro($this->input->post());
+		if($data  == false)
+		{
+			echo json_encode(false);
+		}
+		else
+		{
+			echo json_encode(true);	
+		}
+	}
+
+	//SubRubro
+	public function indexSR($permission)
+	{
+		$data['list'] = $this->Rubros->SubRubro_List();
+		$data['permission'] = $permission;
+		echo json_encode($this->load->view('rubros/listSR', $data, true));
+	}
+	
+	public function getSubRubro(){
+		$data['data'] = $this->Rubros->getSubRubro($this->input->post());
+		$response['html'] = $this->load->view('rubros/viewSR_', $data, true);
+		echo json_encode($response);
+	}
+
+	public function setSubRubro(){
+		$data = $this->Rubros->setSubRubro($this->input->post());
 		if($data  == false)
 		{
 			echo json_encode(false);
--- application/models/Rubros.php
+++ application/models/Rubros.php
@@ -107,5 +107,119 @@ class Rubros extends CI_Model
 
 		}
 	}
+
+	#region subrubro
+	function SubRubro_List(){
+		$this->db->select('subrubros.*, rubros.rubDescripcion');
+		$this->db->from('subrubros');
+		$this->db->join('rubros', ' rubros.rubId = subrubros.rubId');
+		$this->db->order_by('subrDescripcion', 'asc');
+		$query = $this->db->get(); 
+		
+		if ($query->num_rows()!=0)
+		{
+			return $query->result_array();	
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	function getSubRubro($data = null){
+		if($data == null)
+		{
+			return false;
+		}
+		else
+		{
+			$action = $data['act'];
+			$idRub = $data['id'];
+			$data = array();
+
+			//Datos del rubro
+			$query= $this->db->get_where('subrubros',array('subrId'=>$idRub));
+			if ($query->num_rows() != 0)
+			{
+				$c = $query->result_array();
+				$data['subrubro'] = $c[0];
+
+			} else {
+				$art = array();
+				$art['subrId'] = '';
+				$art['subrDescripcion'] = '';
+				$art['rubId'] = -1;
+				$art['subrEstado'] = '';
+				$data['subrubro'] = $art;
+			}
+			$data['subrubro']['action'] = $action;
+
+			//Readonly
+			$readonly = false;
+			if($action == 'Del' || $action == 'View'){
+				$readonly = true;
+			}
+			$data['read'] = $readonly;
+			$data['action'] = $action;
+
+			//Tipos de Documento
+			$this->db->order_by('rubDescripcion');
+			$query= $this->db->get_where('rubros',array('rubEstado' => 'AC'));
+			if ($query->num_rows() != 0)
+			{
+				$data['rubros'] = $query->result_array();	
+			}
+			
+			return $data;
+		}
+	}
+
+	function setSubRubro($data = null){
+		if($data == null)
+		{
+			return false;
+		}
+		else
+		{
+			$id 	= $data['id'];
+            $act 	= $data['act'];
+            $name 	= $data['name'];
+            $status = $data['status'];
+            $rubId  = $data['rbId'];
+            
+
+			$data = array(
+				   'subrDescripcion' 				=> $name,
+				   'rubId'							=> $rubId,
+				   'subrEstado'						=> $status				   
+				);
+
+			switch($act){
+				case 'Add':
+					//Agregar subrubro 
+					if($this->db->insert('subrubros', $data) == false) {
+						return false;
+					} 
+					break;
+				
+				 case 'Edit':
+				 	//Actualizar subrubro
+				 	if($this->db->update('subrubros', $data, array('subrId'=>$id)) == false) {
+				 		return false;
+				 	}
+				 	break;
+					
+				 case 'Del':
+				 	//Eliminar subrubro
+				 	if($this->db->delete('subrubros', array('subrId'=>$id)) == false) {
+				 		return false;
+				 	}
+				 	break;
+				 	
+			}
+			return true;
+
+		}
+	}
 }
 ?>
