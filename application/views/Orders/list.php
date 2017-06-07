67335950af3576eabe08eb645fb6437c92a84dc9
Olivera Mauricio <feboxp@gmail.com>
2017-06-07 19:11:54 -0300

    - cambio de  Idioma datatable views/Orders/list.php

--- application/views/Orders/list.php
+++ application/views/Orders/list.php
@@ -43,6 +43,32 @@
 
 <script>
   $(function () {
+
+    var datatable_es={
+    "sProcessing":     "Procesando...",
+    "sLengthMenu":     "Mostrar _MENU_ registros",
+    "sZeroRecords":    "No se encontraron resultados",
+    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+    "sInfoPostFix":    "",
+    "sSearch":         "Buscar:",
+    "sUrl":            "",
+    "sInfoThousands":  ",",
+    "sLoadingRecords": "Cargando...",
+    "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+    },
+    "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+    }
+};
+
     var dataTable= $('#order_table').DataTable({
       "processing": true,
       "serverSide": true,
@@ -52,6 +78,7 @@
       "ordering": true,
       "info": true,
       "autoWidth": true,
+      "language":datatable_es,
       'ajax':{
           "dataType": 'json',
           //"contentType": "application/json; charset=utf-8",
@@ -115,6 +142,7 @@
             console.debug(the_error);
           }
         }
+
     });
   });
 
